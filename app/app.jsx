8bd29c6279465cee2561e000ee87213b56632756
Mahdieh Melati <m.melati@etick.local>
2018-09-09 11:49:49 +0430

    add foudation and jQuery

--- app/app.jsx
+++ app/app.jsx
@@ -4,7 +4,12 @@ var {Route,Router,IndexRoute,hashHistory}= require("react-router");
 var Main=require('Main')
 var Weather= require('Weather');
 var About =require('About');
-var Examples= require('Examples')
+var Examples= require('Examples');
+
+//Load foundation
+require('style!css!foundation-sites/dist/foundation.min.css');
+$(document).foundation();
+
 ReactDOM.render(
   <Router history={hashHistory}>
     <Route path="/" component={Main}>
