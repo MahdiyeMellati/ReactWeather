a0ab8bedd75f9034c55b9065b8eacc779563fe51
Mahdieh Melati <m.melati@etick.local>
2018-09-09 14:40:06 +0430

    Style Nav bar

--- app/components/Nav.jsx
+++ app/components/Nav.jsx
@@ -1,5 +1,63 @@
 var React = require("react")
 var {Link,IndexLink}=require('react-router');
+
+
+  var Nav= React.createClass({
+    onSearch: function(e)
+    {
+      e.preventDefault();
+      alert('nothig wired up yet!')
+    },
+    render: function(){
+      return(
+        <div className="top-bar">
+          <div className="top-bar-left">
+            <ul className="menu">
+              <li className-="menu-text">React Weather App</li>
+              <li><IndexLink to="/" activeClassName="active" activeStyle={{fronweight: 'bold'}}> Get Weather</IndexLink></li>
+              <li><Link to="/about" activeClassName="active" activeClassName="active"  activeStyle={{fronweight: 'bold'}}>About</Link></li>
+              <li><Link to="/Examples" activeClassName="active" activeStyle={{fronweight: 'bold'}}>Examples</Link></li>
+            </ul>
+          </div>
+
+          <div className="top-bar-right" >
+
+              <form onSubmit={this.onSearch}>
+                <ul className="menu">
+                <li>
+                  <input type="search" placeholder="Search weather..."/>
+                </li>
+
+                <li>
+                  <input type="submit" className="button" value="GetWeather"/>
+                </li>
+                </ul>
+              </form>
+
+          </div>
+
+        </div>
+
+      );
+    }
+  });
+module.exports =Nav;
+
+// var oldNav=
+// {
+//   <div>
+//     <h3>Nav Component!</h3>
+//   <div><IndexLink to="/" activeClassName="active" activeStyle={{fronweight: 'bold'}}> Get Weather</IndexLink></div>
+//     <div><IndexLink to="/about" activeClassName="active" activeClassName="active"  activeStyle={{fronweight: 'bold'}}> About</IndexLink></div>
+//   <div><IndexLink to="/Examples" activeClassName="active" activeStyle={{fronweight: 'bold'}}>Examples</IndexLink></div>
+//    <a href="#/about">go to about</a>
+//
+//   </div>
+// }
+
+
+
+
 // var Nav= React.createClass({
 //   render: function(){
 //     return(
@@ -14,19 +72,3 @@ var {Link,IndexLink}=require('react-router');
 //
 //     );
 //   }
-
-
-  var Nav= (props)=>{
-    return(
-      <div>
-        <h3>Nav Component!</h3>
-      <div><IndexLink to="/" activeClassName="active" activeStyle={{fronweight: 'bold'}}> Get Weather</IndexLink></div>
-        <div><IndexLink to="/about" activeClassName="active" activeClassName="active"  activeStyle={{fronweight: 'bold'}}> About</IndexLink></div>
-      <div><IndexLink to="/Examples" activeClassName="active" activeStyle={{fronweight: 'bold'}}>Examples</IndexLink></div>
-       <a href="#/about">go to about</a>
-
-      </div>
-
-    );
-  };
-module.exports =Nav;
